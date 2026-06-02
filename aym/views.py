ffdb6b5a9ff71e873eaf5227a9b45daebaba9ba1
Walfenix <matimaticastro@gmail.com>
2026-06-02 12:01:05 -0400

    added a way to view products

--- aym/views.py
+++ aym/views.py
@@ -1,6 +1,8 @@
-from urllib import request
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+import json
+from pathlib import Path
+
+from django.conf import settings
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
@@ -10,4 +12,21 @@ def login(request):
     return render(request, 'aym/login.html')
 
 def tables(request):
-    return render(request, 'aym/tables.html')
+    product_file = Path(settings.BASE_DIR) / 'static' / 'productos.json'
+    products = []
+
+    if product_file.exists():
+        try:
+            with product_file.open('r', encoding='utf-8') as f:
+                data = json.load(f)
+            products = [
+                {
+                    'pk': item.get('pk'),
+                    **item.get('fields', {}),
+                }
+                for item in data
+            ]
+        except (json.JSONDecodeError, OSError):
+            products = []
+
+    return render(request, 'aym/tables.html', {'products': products})
